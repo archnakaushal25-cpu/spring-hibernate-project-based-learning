e903d25957e19182b543a9117dab939db864bba1
archnakaushal25-cpu <archnakaushal25@gmail.com>
2025-11-05 13:35:11 +0530

    Create HibernateUtil.java

---
+++ src/main/java/com/nimbus/crud/config/HibernateUtil.java
@@ -0,0 +1,18 @@
+package com.nimbus.crud.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() { return sessionFactory; }
+}
