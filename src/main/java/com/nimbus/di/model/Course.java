69c3d0afab7f2d6d337d71687a85d9f449ce9821
archnakaushal25-cpu <archnakaushal25@gmail.com>
2025-11-05 13:31:35 +0530

    Create Course.java

---
+++ src/main/java/com/nimbus/di/model/Course.java
@@ -0,0 +1,7 @@
+package com.nimbus.di.model;
+
+public class Course {
+    private String courseName;
+    public Course(String courseName) { this.courseName = courseName; }
+    public String getCourseName() { return courseName; }
+}
